eaa59c3928f34b7725e16ad0aaf2887154602a0a
kirichenkods <kirichenkods@ya.ru>
2022-05-09 15:27:23 +0300

    add autoload

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hello, Hexlet!';
+// $autoloadPath1 = __DIR__ . '/home/omega/.config/composer/vendor/autoload.php';
+
+// $autoloadPath2 = __DIR__ . '/vendor/autoload.php';
+
+// if (file_exists($autoloadPath1)) {
+//     require_once $autoloadPath1;
+// } else {
+//     require_once $autoloadPath2;
+// }
+
+// echo 'Hello, Hexlet!';
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
